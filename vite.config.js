a055c5e2dd145f14cff76c22863b4ce254f1000f
Arthur <agavrik464@gmail.com>
2025-02-25 13:40:11 +0200

    changed swc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/movieCast/', // Замени на имя твоего репозитория на GitHub
   plugins: [react()],
 })
