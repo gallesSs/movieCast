ec7a2c6f2c7dab4e2d5174074e6a7c0f1bb6826d
Arthur <agavrik464@gmail.com>
2025-02-25 13:30:34 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/movieCast/', // Замени на имя твоего репозитория на GitHub
   plugins: [react()],
 })
